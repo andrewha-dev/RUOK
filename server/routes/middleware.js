8d9737dafec9a3dcbd90fb1ac4fd512232c18e80
Solunra <sunnydeng90@gmail.com>
2020-01-26 08:57:11 -0500

    fixed authentication

--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -2,7 +2,7 @@ const requireAuth = (req, res, next) =>
   !req.user ? res.status(401).send({ message: 'User not authenticated' }) : next();
 
 const checkAdminAccess = (req, res, next) =>
-    !req.user.isAdmin ? res.status(401).send({ message: 'User not authenticated' }) : next();
+    !req.user || typeof(req.user.isAdmin) != 'undefined' ? res.status(401).send({ message: 'User not authenticated' }) : next();
 
 module.exports = {
   requireAuth,
