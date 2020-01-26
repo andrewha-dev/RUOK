731d98f8348b9d4f1c4f898e3d9bf8526a84dc58
Ahnaf Habib Khan <ahnafkhan70@gmail.com>
2020-01-25 21:58:39 -0500

    css changes

--- client/components/organisms/Tips/Tips.js
+++ client/components/organisms/Tips/Tips.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './Tips.css';
 const Tips = () => {
         return(
-         <div>
-         <h3>Hey, you don't have a survey today. <br/> <br/>
-         Here are some tips to avoid stress:</h3>
-         <br/>
-                <ul>
-                       
-                        <li>1. Avoid Caffeine, Alcohol, and Nicotine.</li> 
-                        <li>2. Indulge in Physical Activity</li> 
-                        <li>3. Get More Sleep </li> 
-                        <li>4. Try Relaxation Techniques </li> 
+         <div className="TipsContainer">
+         <h3 className="TipsHeaders">Hey, you don't have a survey today!!! </h3> 
+                <p className="TipsHeaders">
+                        Here are some tips to avoid stress:
+                </p>
+                <ul className="TipsUL">
+
+                        <li className="TipsList">1. Avoid Caffeine, Alcohol, and Nicotine.</li> 
+                        <li className="TipsList">2. Indulge in Physical Activity</li> 
+                        <li className="TipsList">3. Get More Sleep </li> 
+                        <li className="TipsList">4. Try Relaxation Techniques </li> 
                 </ul> 
         </div>
         )
