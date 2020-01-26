5a88bebaf511e9ff0fb858079884d1f1c8ab106a
Sunny Deng <43704440+Solunra@users.noreply.github.com>
2020-01-26 11:12:28 -0500

    Add finishing touches (#4)
    * Added basic survey
    
    * fml quick fix
    
    * Back-end calculation
    
    Co-authored-by: Andrew Ha <34655105+andrewha-dev@users.noreply.github.com>

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -2,13 +2,14 @@ const express = require("express");
 const { requireAuth } = require('./middleware');
 const { Survey } = require('../database/schemas');
 const { Answer } = require('../database/schemas');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
 module.exports = router;
 
 router.get("/", requireAuth, (req, res) => {
-    Survey.find({}, (err, questions) => {
+    Survey.find().distinct('question', (err, questions) => {
         if(err) {
             res.status(400).send({message: "No questions found"});
         }
@@ -19,13 +20,30 @@ router.get("/", requireAuth, (req, res) => {
 });
 
 router.post("/", requireAuth, (req, res) => {
-    const newAnswer = new Answer(req.body.completedSurvey);
-    newAnswer.save(savedAnswer => {
-        if(!req.body.identity) {
-            res.status(400).send({message: "Unable to record answer, team name"});
-        }
-        res.status(200).send({message: "Answer has been recorded", answer: savedAnswer});
-    }).catch(err => {
-        res.status(400).send({message: "Unable to record answer"});
-    });
+    let answers = req.body.answers;
+    console.log(answers);
+    let score = 0;
+    for( let index = 0; index < answers.length ; ++index ){
+        score += answers[index].value;
+    }
+    const answerModel = mongoose.model("Answer");
+    const newAnswer = new answerModel;
+
+    if(!req.user) {
+        res.status(400).send({message: "Unable to record answer, requires team name"});
+    }
+    else {
+        newAnswer.identity = req.user.username_case;
+        newAnswer.points = score;
+        newAnswer.added = Date.now();
+
+        newAnswer.save((err, savedAnswer) => {
+            if (err) {
+                res.status(400).send({message: "Unable to record answer"});
+            }
+            else {
+                res.status(200).send({message: "Answer has been recorded", answer: savedAnswer});
+            }
+        });
+    }
 });
