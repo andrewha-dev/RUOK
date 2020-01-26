71d3b6654e1aa2f3fa50342fc90dd0d3a07137d2
Sunny Deng <43704440+Solunra@users.noreply.github.com>
2020-01-26 14:47:11 -0500

    Development (#7)
    * Fixed npm run script issues
    
    * uwu
    
    * owo
    
    * ewe
    
    * qwq
    
    * Fixed bad bugs
    
    * Sorry Sunny
    
    * Dropdown fix
    
    * Bug fix
    
    * fuck
    
    * Fixed survey
    
    * fuck
    
    * tips
    
    * CSS
    
    * Uwubranch (#1)
    
    * ewe
    
    * Update test route
    
    * no error means no problems
    
    * css changes
    
    * changes in css
    
    * rofl
    
    * qwq
    
    * mongoose add
    
    * Added mini-HR sim (#2)
    
    * Added admin integration
    
    * fixed authentication
    
    * Add finishing touches (#4)
    
    * Added basic survey
    
    * fml quick fix
    
    * Back-end calculation
    
    Co-authored-by: Andrew Ha <34655105+andrewha-dev@users.noreply.github.com>
    
    * Add get answers
    
    * fix
    
    * updated test values
    
    * Add HR Screen Demo (#6)
    
    Co-authored-by: Ahnaf Khan <36949152+ahnafhabibkhan@users.noreply.github.com>
    
    Co-authored-by: Andrew Ha <34655105+andrewha-dev@users.noreply.github.com>
    Co-authored-by: Ahnaf Khan <36949152+ahnafhabibkhan@users.noreply.github.com>

--- client/components/pages/HomePage/HomePage.js
+++ client/components/pages/HomePage/HomePage.js
@@ -4,16 +4,51 @@ import { push } from 'connected-react-router';
 import Survey from '../../organisms/Survey/index'
 import Tips from '../../organisms/Tips/Tips'
 import * as R from 'ramda';
+import {getResults} from '../../../api/answer'
+import { get } from 'mongoose';
 
 export default function HomePage() {
   const dispatch = useDispatch();
   const { user } = useSelector(R.pick(['user']));
   const [surveyAvailable, setSurveyAvailable] = useState(true);
+  const [answers, setAnswers] = useState([]);
+
   useEffect(() => {
     if (R.isEmpty(user)) {
       dispatch(push('/login'));
     }
+
+    getResults().then(res=>{
+      console.log("Coming from res", res)
+      console.log(res["question"]);
+      setAnswers(res["question"])
+    });
   }, []);
+
+  const HR=()=>{
+    let test = [];
+
+
+    console.log("Answers: inside Hr ", answers);
+    return (<div>
+            {answers.forEach(val => {
+              <p>{val.identity}</p>
+            })}
+      </div>
+      )
+  }
+console.log(user);
+const Wrapper=()=>{
+
+if(user){
+if(user.isAdmin){
+    return <HR/>
+  }
+  else
+    return <Display/>
+}
+  
+  }
 
 
   const Display = () => {
@@ -33,7 +68,8 @@ export default function HomePage() {
       <div className="section">
         <div className="container">
           <h1 className="title is-1">
-            <Display/>
+            <Wrapper/>
+
           </h1>
         </div>
       </div>
--- server/routes/middleware.js
+++ server/routes/middleware.js
@@ -2,7 +2,8 @@ const requireAuth = (req, res, next) =>
   !req.user ? res.status(401).send({ message: 'User not authenticated' }) : next();
 
 const checkAdminAccess = (req, res, next) =>
-    !req.user || typeof(req.user.isAdmin) != 'undefined' ? res.status(401).send({ message: 'User not authenticated' }) : next();
+    !req.user || typeof(req.user.isAdmin) == 'undefined' || req.user.isAdmin === false ? res.status(401).send({ message: 'User not authenticated' }) : next();
+
 
 module.exports = {
   requireAuth,
